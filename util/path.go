40d46df13f511b4da6dafafba53e35c65635e795
Karl <465620111@qq.com>
2022-11-02 16:38:51 +0800

    time

---
+++ util/path.go
@@ -0,0 +1,14 @@
+package util
+
+import "os"
+
+func PathExist(fullPath string) bool {
+	_, err := os.Stat(fullPath)
+	if err == nil {
+		return true
+	}
+	if os.IsNotExist(err) {
+		return false
+	}
+	return false
+}
---
+++ util/time.go
@@ -0,0 +1,19 @@
+package util
+
+import "time"
+
+func NowString() string {
+	return time.Now().Format("2006-01-02 15:04:05")
+}
+
+func TimeFromString(str string) (time.Time, error) {
+	return time.ParseInLocation("2006-01-02 15:04:05", str, time.Local)
+}
+
+func TimestampToString(ts int64) string {
+	return time.Unix(ts, 0).Format("2006-01-02 15:04:05")
+}
+
+func TimeToString(t time.Time) string {
+	return t.Format("2006-01-02 15:04:05")
+}
